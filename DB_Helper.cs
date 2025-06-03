f60ff9d30834ee84597bc1db14cf0f4376bdf8c9
maxundeli <68638365+maxundeli@users.noreply.github.com>
2025-06-04 03:07:21 +0500

    Optimize database update logic

--- DB_Helper.cs
+++ DB_Helper.cs
@@ -51,105 +51,37 @@ public class MyDatabaseHelper
     public async Task UpdateStat(string id, int kills, int damageDealed, TimeSpan timeplayed, int FFkillsCount,
         int takedSCPObjects, int SCPsKilled)
     {
-        using (var conn = new MySqlConnection(connectionString))
+        using var conn = new MySqlConnection(connectionString);
+        try
         {
-            try
-            {
-                await conn.OpenAsync();
-                var cmd1 = new MySqlCommand(
-                    "SELECT kills FROM scp_stat WHERE ID = @userId;",
-                    conn
-                );
-                cmd1.Parameters.AddWithValue("@userId", id);
-                object result = await cmd1.ExecuteScalarAsync();
-                int killsTemp = result != null ? Convert.ToInt32(result) : 0;
+            await conn.OpenAsync();
 
+            var cmd = new MySqlCommand(
+                @"UPDATE scp_stat
+                  SET kills = kills + @kills,
+                      damageDealed = damageDealed + @damageDealed,
+                      timePlayed = ADDTIME(timePlayed, @timePlayed),
+                      FFkills = FFkills + @FFkillsCount,
+                      takedSCPObjects = takedSCPObjects + @takedSCPObjects,
+                      SCPsKilled = SCPsKilled + @SCPsKilled
+                  WHERE ID = @userId;",
+                conn
+            );
 
-                var cmd3 = new MySqlCommand(
-                    "SELECT damageDealed FROM scp_stat WHERE ID = @userId;",
-                    conn
-                );
-                cmd3.Parameters.AddWithValue("@userId", id);
-                object result2 = await cmd3.ExecuteScalarAsync();
-                int damageTemp = result2 != null ? Convert.ToInt32(result2) : 0;
-                int damageFinal = damageTemp + damageDealed;
-                int killsFinal = kills + killsTemp;
+            cmd.Parameters.AddWithValue("@kills", kills);
+            cmd.Parameters.AddWithValue("@damageDealed", damageDealed);
+            cmd.Parameters.AddWithValue("@timePlayed", timeplayed);
+            cmd.Parameters.AddWithValue("@FFkillsCount", FFkillsCount);
+            cmd.Parameters.AddWithValue("@takedSCPObjects", takedSCPObjects);
+            cmd.Parameters.AddWithValue("@SCPsKilled", SCPsKilled);
+            cmd.Parameters.AddWithValue("@userId", id);
 
-
-                var cmd2 = new MySqlCommand(
-                    "UPDATE scp_stat SET kills = @killsFinal WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd2.Parameters.AddWithValue("@userId", id);
-                cmd2.Parameters.AddWithValue("@killsFinal", killsFinal);
-                await cmd2.ExecuteNonQueryAsync();
-
-
-                var cmd4 = new MySqlCommand(
-                    "UPDATE scp_stat SET damageDealed = @damageFinal WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd4.Parameters.AddWithValue("@userId", id);
-                cmd4.Parameters.AddWithValue("@damageFinal", damageFinal);
-                await cmd4.ExecuteNonQueryAsync();
-
-
-                var cmd5 = new MySqlCommand(
-                    "SELECT timePlayed FROM scp_stat WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd5.Parameters.AddWithValue("@userId", id);
-                object result3 = await cmd5.ExecuteScalarAsync();
-                string timePlayedTemp = result3 != null ? Convert.ToString(result3) : "00:00:00";
-                var timePlayedTempSpan = TimeSpan.Parse(timePlayedTemp);
-                Log.Info("Time played: " + timePlayedTempSpan);
-                await cmd5.ExecuteNonQueryAsync();
-                var timePlayedFinal = timePlayedTempSpan + timeplayed;
-
-                var cmd6 = new MySqlCommand(
-                    "UPDATE scp_stat SET timePlayed = @timePlayed WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd6.Parameters.AddWithValue("@userId", id);
-                cmd6.Parameters.AddWithValue("@timePlayed", Convert.ToString(timePlayedFinal));
-                await cmd6.ExecuteNonQueryAsync();
-
-                var cmd7 = new MySqlCommand(
-                    "UPDATE scp_stat SET FFkills = @FFkillsCount + FFkills WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd7.Parameters.AddWithValue("@userId", id);
-                cmd7.Parameters.AddWithValue("@FFkillsCount", FFkillsCount);
-                await cmd7.ExecuteNonQueryAsync();
-
-                var cmd8 = new MySqlCommand(
-                    "UPDATE scp_stat SET takedSCPObjects = @takedSCPObjects + takedSCPObjects WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd8.Parameters.AddWithValue("@userId", id);
-                cmd8.Parameters.AddWithValue("@takedSCPObjects", takedSCPObjects);
-                await cmd8.ExecuteNonQueryAsync();
-
-                var cmd9 = new MySqlCommand(
-                    "UPDATE scp_stat SET SCPsKilled = @SCPsKilled + SCPsKilled WHERE ID = @userId;"
-                    ,
-                    conn
-                );
-                cmd9.Parameters.AddWithValue("@userId", id);
-                cmd9.Parameters.AddWithValue("@SCPsKilled", SCPsKilled);
-                await cmd9.ExecuteNonQueryAsync();
-            }
-            catch (Exception e)
-            {
-                Log.Error(e);
-                throw;
-            }
+            await cmd.ExecuteNonQueryAsync();
+        }
+        catch (Exception e)
+        {
+            Log.Error(e);
+            throw;
         }
     }
 }
